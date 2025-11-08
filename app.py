ac944ecca4012b3f81f0ff7bebc7c26403f43743
Felixby2004 <andree.felix2004@gmail.com>
2025-11-07 23:42:52 -0500

    cambio

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from utils.data import init_session_state
 st.set_page_config(
     page_title="El Establo",
     page_icon="🥩",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
+    layout="wide"
 )
 
 
