53ada3e94b7cc7b335a314c8cba95d9dfd07cba0
Felixby2004 <andree.felix2004@gmail.com>
2025-11-08 12:50:23 -0500

    in

--- views/ventas.py
+++ views/ventas.py
@@ -147,7 +147,7 @@ def show():
 
             # Limpiar carrito y observaciones
             st.session_state.cart = []
-            st.session_state.observaciones_input = ""  # limpiar correctamente
+            st.session_state.observaciones_input = " "  # limpiar correctamente
             st.success(f"✅ Venta registrada correctamente (ID: {venta_id})")
 
             st.rerun()
