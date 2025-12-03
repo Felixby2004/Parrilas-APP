6428bd0a460cc4a1ab2457d10b73cd3a0f71d73c
Felixby2004 <andree.felix2004@gmail.com>
2025-12-03 11:47:46 -0500

    11

--- views/carta.py
+++ views/carta.py
@@ -1,22 +1,12 @@
 # pages/carta.py
 import streamlit as st
-from utils.data import PLATOS, BEBIDAS
 
 def show():
     st.header("📜 Carta")
-    col1, col2 = st.columns(2)
-    with col1:
-        st.subheader("Platos")
-        for name, price in PLATOS.items():
-            st.write(f"**{name}** — S/. {price:.2f}")
-    with col2:
-        st.subheader("Bebidas")
-        for name, price in BEBIDAS.items():
-            st.write(f"**{name}** — S/. {price:.2f}")
 
     st.markdown(
-        '<a href="https://www.canva.com/design/DAG56xiL4hw/P-wPRfxuGHtgF37Dxx0aEw/edit" target="_blank">Visitar página</a>',
+        '<a href="https://www.canva.com/design/DAG56xiL4hw/P-wPRfxuGHtgF37Dxx0aEw/edit" target="_blank">Editar carta</a>',
         unsafe_allow_html=True
     )
 
-    st.image("CARTA.png", caption="Carta de parrillas", width=300)
+    st.image("CARTA.png", width=500)
